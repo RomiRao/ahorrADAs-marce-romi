c13598f469a352f78c8122cd078d56769efe78e9
RomiRao <raoromina96@gmail.com>
2023-08-22 17:35:32 -0300

    arreglo funcuon para mostrar fecha del dia

--- script/script.js
+++ script/script.js
@@ -2,15 +2,16 @@ const $ = (selector) => document.getElementById(selector);
 let hoy = new Date();
 
 //Definiendo fecha actual
-window.onload = function () {
-    var fecha = new Date();
-    var mes = fecha.getMonth() + 1;
-    var dia = fecha.getDate();
-    var ano = fecha.getFullYear();
+window.onload = () => {
+    let fecha = new Date();
+    let mes = fecha.getMonth() + 1;
+    let dia = fecha.getDate();
+    let anio = fecha.getFullYear();
     if (dia < 10) dia = "0" + dia;
     if (mes < 10) mes = "0" + mes;
-    document.getElementById("fecha").value = ano + "-" + mes + "-" + dia;
-    document.getElementById("fecha-filtro").value = ano + "-" + mes + "-" + dia;
+    document.getElementById("fecha").value = anio + "-" + mes + "-" + dia;
+    document.getElementById("fecha-filtro").value =
+        anio + "-" + mes + "-" + dia;
 };
 
 // *****************
