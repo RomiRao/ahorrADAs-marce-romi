1dec020de503538758ee0beafaffe8cdc74bccf1
RomiRao <raoromina96@gmail.com>
2023-08-22 17:18:33 -0300

    hago que se muestre la fecha del dia en el input fecha

--- script/script.js
+++ script/script.js
@@ -1,4 +1,17 @@
 const $ = (selector) => document.getElementById(selector);
+let hoy = new Date();
+
+//Definiendo fecha actual
+window.onload = function () {
+    var fecha = new Date();
+    var mes = fecha.getMonth() + 1;
+    var dia = fecha.getDate();
+    var ano = fecha.getFullYear();
+    if (dia < 10) dia = "0" + dia;
+    if (mes < 10) mes = "0" + mes;
+    document.getElementById("fecha").value = ano + "-" + mes + "-" + dia;
+    document.getElementById("fecha-filtro").value = ano + "-" + mes + "-" + dia;
+};
 
 // *****************
 // NAVBAR
