e6ad8d769c033dc138bad8af9b45186b578ec60d
Makorii <marcelaromina.d@hotmail.com>
2023-08-26 13:23:24 -0300

    arreglo el array a un array de objetos, agrego id a los botones de editar y eliminar y agrego funcion eliminarCategoria

--- script/script.js
+++ script/script.js
@@ -51,20 +51,62 @@ $("nueva-operacion-btn").addEventListener("click", () => abrirNuevaOperacion());
 
 // ------------Funcionabilidad Categorias------------------
 
-const categorias = ["Comida", "Servicios", "Salidas", "Educación", "Transporte", "Trabajo"];
+// const categorias = ["Comida", "Servicios", "Salidas", "Educación", "Transporte", "Trabajo"];
+const randomId = () => self.crypto.randomUUID();
+
+let categorias = [
+    {
+        id: randomId(),
+        nombre: "Comida"
+    },
+    {
+        id: randomId(),
+        nombre: "Servicios"
+    },
+    {
+        id: randomId(),
+        nombre: "Salidas"
+    },
+    {
+        id: randomId(),
+        nombre: "Educación"
+    },
+    {
+        id: randomId(),
+        nombre: "Transporte"
+    },
+    {
+        id: randomId(),
+        nombre: "Trabajo"
+    }
+]
+
+const $$ = (selector) => document.querySelectorAll(selector)
 
 const crearLista = (listaDeCategorias) => {
     $("lista-categorias").innerHTML = "";
     for (let categoria of listaDeCategorias){
         $("lista-categorias").innerHTML += `
-        <li class="is-flex is-justify-content-space-between">
-            <span class="tag is-primary is-light">${categoria}</span>
+        <li  class="is-flex is-justify-content-space-between">
+            <span class="tag is-primary is-light">${categoria.nombre}</span>
             <div class="has-text-right">
-                <a href="#" class="is-size-7 mr-4 editarBtn" >Editar</a>
-                <a href="#" class="is-size-7 eliminarBtn">Eliminar</a>
+                <a href="#" id="${categoria.id}" class="is-size-7 mr-4 editarBtn" >Editar</a>
+                <a href="#" id="${categoria.id}" class="is-size-7 eliminarBtn">Eliminar</a>
             </div>
         </li>`
     }
 }
 
 crearLista(categorias);
+
+const eliminarCategoria = (id) => {
+    let newArray = categorias.filter((categoria) => categoria.id !== id);
+    crearLista(newArray);
+    console.log(newArray);
+}
+
+const eliminarBtn = document.querySelectorAll(".eliminarBtn");
+
+eliminarBtn.forEach((btn) =>
+    btn.addEventListener("click", () => eliminarCategoria(btn.id))
+);
