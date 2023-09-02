b03ee8ddc9c312934dbee7378c73206b54f31f88
Makorii <marcelaromina.d@hotmail.com>
2023-09-02 11:09:54 -0300

    arreglo error que me recargaba la pag sola, agrego funcion mostrar y ocultar vista editar categoria, agrego funcion editar categoria

--- script/script.js
+++ script/script.js
@@ -81,7 +81,7 @@ const agregarOperacion = (objeto) => {
 
 //definiendo el valor del input fecha
 const fechaElegida = () => {
-    operacionFecha = new Date($("fecha").value);
+    let operacionFecha = new Date($("fecha").value);
     let mes = operacionFecha.getMonth() + 1;
     let dia = operacionFecha.getDate();
     let anio = operacionFecha.getFullYear();
@@ -164,9 +164,9 @@ const iterarOperaciones = (listaOperaciones) => {
                 mostrarOperaciones(operaciones);
             })
         );
-        $$(".editar-link").forEach((boton) =>
-            boton.addEventListener("click", () => editarOperacion(boton.id))
-        );
+        // $$(".editar-link").forEach((boton) =>
+        //     boton.addEventListener("click", () => editarOperacion(boton.id))
+        // );
     });
     actualizarInfo("operaciones", operaciones);
     noHayOperaciones();
@@ -238,17 +238,7 @@ let categorias = [
 ];
 
 
-//-----Agregar nueva Categoria
 
-const agregarCategoria = () => {
-    let nuevoObj = {
-        id: randomId(),
-        nombre: $("input-nueva-categoria").value,
-    };
-    let listaActualizada = [...categorias, nuevoObj];
-    crearLista(listaActualizada);
-};
-$("boton-agregar-categoria").addEventListener("click", () => agregarCategoria);
 
 //------Crear lista Categorias
 
@@ -266,6 +256,21 @@ const crearLista = (listaDeCategorias) => {
     }
 };
 
+mostrarOperaciones(operaciones);
+
+//-----Agregar nueva Categoria
+
+const agregarCategoria = () => {
+    let nuevoObj = {
+        id: randomId(),
+        nombre: $("input-nueva-categoria").value,
+    };
+    let listaActualizada = [...categorias, nuevoObj];
+    crearLista(listaActualizada);
+};
+
+$("boton-agregar-categoria").addEventListener("click", agregarCategoria);
+
 //----Obtener categoria
 const obtenerCategoria = (idCategoria, categorias) => {
     return categorias.find((categoria) => categoria.id === idCategoria);
@@ -273,10 +278,30 @@ const obtenerCategoria = (idCategoria, categorias) => {
 
 //----Mostrar vista editar categoria
 const mostrarEditarCategoria = (id) => {
-    $("container-categorias").classList.remove("is-hidden");
+    $("container-categorias").classList.add("is-hidden");
+    $("editar-categoria").classList.remove("is-hidden");
     let categoriaAEditar = obtenerCategoria(id, categorias);
-    categoriaAEditar.nombre = $("input-editar").value;
+    $("input-editar").value = categoriaAEditar.nombre;
+    $("boton-editar").addEventListener("click", () => editarCategoria(categoriaAEditar.id))
+    ocultarEditarCategoria()
+}
+
+const ocultarEditarCategoria = () => {
+    $("boton-cancelar").addEventListener("click", () => {
+        $("container-categorias").classList.remove("is-hidden");
+        $("editar-categoria").classList.add("is-hidden");
+    })
+}
+
+const editarCategoria = (id) => {
+    let nuevaCategoria = {
+        id: id,
+        nombre: $("input-editar").value
+    }
+    let categoriasActualizadas = categorias.map((categoria) =>
+    categoria.id === id ? { ...nuevaCategoria } : categoria
+    );
+    crearLista(categoriasActualizadas);
 }
 
 crearLista(categorias);
-mostrarOperaciones(operaciones);
